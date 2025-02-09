40616d30f2847ec8e2c1e2ddea32123c0515a4a0
Shadman Shariar <shadmanshariar@Shadmans-MacBook-Pro.local>
2025-02-10 03:08:43 +0600

    src added

---
+++ registration-service/src/main/java/com/user_service/user_service/RegistrationController.java
@@ -0,0 +1,14 @@
+package com.user_service.user_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+    @GetMapping
+    public String registration(){
+        return "Reg";
+    }
+}
